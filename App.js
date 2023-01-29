ae086ab45191161450b95072d200e9cdaf1499f2
aliozer00 <92879108+aliozer00@users.noreply.github.com>
2023-01-29 23:23:32 +0300

    seacrh.js Icons added

--- App.js
+++ App.js
@@ -3,14 +3,48 @@ import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import Home from "./screens/Home";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
 const Tab = createBottomTabNavigator();
+import { Ionicons, AntDesign } from "@expo/vector-icons";
+import Search from "./screens/Search";
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <Home />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Tab.Navigator
+        styles={{
+          flexDirection: "row",
+          margin: 10,
+          marginHorizontal: 30,
+          justifyContent: "space-between",
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarLabel: "Home",
+            tabBarIcon: ({ color, size, textColor }) => (
+              <Ionicons name="home" color="black" size={24} textColor="black" />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Search"
+          component={Search}
+          options={{
+            tabBarLabel: "Browse",
+            tabBarIcon: ({ color, size, textColor }) => (
+              <Ionicons
+                name="search"
+                color="black"
+                size={24}
+                textColor="black"
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
