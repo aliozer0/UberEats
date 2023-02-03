6d0dbb5a60fdbfe09e95a371079d2f568bab90cb
aliozer00 <92879108+aliozer00@users.noreply.github.com>
2023-02-03 20:34:03 +0300

    Modal View Finished

--- components/restaurantDetail/ViewCard.js
+++ components/restaurantDetail/ViewCard.js
@@ -1,6 +1,7 @@
-import { View, Text, TouchableOpacity, Modal } from "react-native";
+import { View, Text, TouchableOpacity, Modal, StyleSheet } from "react-native";
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import OrderItem from "./OrderItem";
 
 export default function ViewCard() {
   const [modalVisible, setModalVisible] = useState(false);
@@ -21,30 +22,79 @@ export default function ViewCard() {
 
   const checkoutModalContent = () => {
     return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          marginTop: 30,
-        }}
-      >
-        <View
-          style={{
-            backgroundColor: "black",
-            padding: 10,
-            borderRadius: 30,
-            width: 150,
-            alignItems: "center",
-          }}
-        >
-          <TouchableOpacity onPress={() => setModalVisible(false)}>
-            <Text style={{ color: "white" }}>Checkout</Text>
-          </TouchableOpacity>
+      <View style={styles.modalContainer}>
+        <View style={styles.modalCheckoutContainer}>
+          <Text style={styles.restaurantName}>{restaurantName}</Text>
+          {items.map((item, index) => (
+            <OrderItem key={index} item={item} />
+          ))}
+          <View style={styles.subtotolContainer}>
+            <Text style={styles.subtotalText}>Subtotal</Text>
+            <Text>{totalUSD}</Text>
+          </View>
+          <View style={{ flexDirection: "row", justifyContent: "center" }}>
+            <TouchableOpacity
+              style={{
+                marginTop: 20,
+                backgroundColor: "black",
+                alignItems: "center",
+                padding: 13,
+                borderRadius: 30,
+                width: 300,
+                position: "relative",
+              }}
+              onPress={() => setModalVisible(false)}
+            >
+              <Text style={{ color: "white", fontSize: 20 }}>Checkout</Text>
+              <Text
+                style={{
+                  position: "absolute",
+                  right: 20,
+                  color: "white",
+                  fontSize: 15,
+                  top: 17,
+                }}
+              >
+                {totalUSD}
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     );
   };
+
+  const styles = StyleSheet.create({
+    modalContainer: {
+      flex: 1,
+      justifyContent: "flex-end",
+      backgroundColor: "rgba(0,0,0,0.7)",
+    },
+    modalCheckoutContainer: {
+      backgroundColor: "white",
+      padding: 16,
+      height: 500,
+      borderWidth: 1,
+    },
+    restaurantName: {
+      textAlign: "center",
+      fontWeight: "bold",
+      fontSize: 18,
+      marginBottom: 10,
+    },
+    subtotolContainer: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      marginTop: 15,
+    },
+    subtotalText: {
+      fontSize: 15,
+      fontWeight: "600",
+      textAlign: "left",
+      marginBottom: 10,
+    },
+  });
+
   return (
     <>
       <Modal
